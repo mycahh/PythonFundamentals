9b0326d79d9df7df6bf03fe4f237287e7d14daa7
mycahh <mikaelfernandezj.d@gmail.com>
2022-09-25 01:29:52 -0400

    initial commit

---
+++ Variables.py
@@ -0,0 +1,9 @@
+float = 3.14
+number = 3
+string = 'The wish'
+bool = True
+
+print(type(float)) # Class Float
+print(type(number)) # Class Number
+print(type(string)) # Class String
+print(type(bool)) # Class Bool
