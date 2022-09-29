d0dc6dd15476f65237b70ee91af528444ae3200d
mycahh <mikaelfernandezj.d@gmail.com>
2022-09-29 15:33:34 -0400

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -3,7 +3,11 @@ number = 3
 string = 'The wish'
 bool = True
 
+none = None
+
 print(type(float)) # Class Float
 print(type(number)) # Class Number
 print(type(string)) # Class String
 print(type(bool)) # Class Bool
+
+print(type(none)) # Class None
